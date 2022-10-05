1c2e38a349d8cf90b6018481de7fef8486a9c155
tim <timoth.david@gmail.com>
2022-10-05 16:52:44 +0200

    add new title

--- vue.config.js
+++ vue.config.js
@@ -6,3 +6,12 @@ module.exports = defineConfig({
     port: 9090,
   },
 });
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "assureplus";
+      return args;
+    });
+  },
+};
