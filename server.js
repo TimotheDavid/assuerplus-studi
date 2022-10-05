e670f0565ae9d0d616367536a43bbb5094c04f0e
tim <timoth.david@gmail.com>
2022-10-05 15:43:58 +0200

    add dotenv

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
+require("dotenv").config();
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 9090;
-app.listen(port);
+app.listen(port, () => {
+  console.log(`listen on port ${process.env.PORT}`);
+});
